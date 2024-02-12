1edeb5a52cef1049ef19b18ec11c3d8f0a212ce0
MSulSal <msulsaleem@gmail.com>
2024-02-11 17:51:27 -0800

    finish navbar, add model and loading

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -11,14 +11,19 @@ const Hero = () => {
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]" />
-          <div className="w-1 sm:h-80 h-40 violet-gradient" />
+          <div className="w-1 sm:h-80 h-80 violet-gradient" />
         </div>
         <div>
           <h1 className={`${styles.heroHeadText} text-white`}>
             Hi, I'm <span className="text-[#915eff]">Suleman</span>
           </h1>
+          <p className={`${styles.heroSubText} mt-2 text-white-100`}>
+            I develop user interfaces and <br />
+            applications for the web and mobile
+          </p>
         </div>
       </div>
+      <ComputersCanvas />
     </section>
   );
 };
