9d129d2c9399104b596f18518fca4babc4f695c6
MSulSal <msulsaleem@gmail.com>
2025-01-22 13:48:33 -0600

    modify nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
 export default function Home() {
-  return (
-    <main className="flex flex-col items-center justify-between p-24">
-      <h1 className="text-4xl font-bold">Hello World!</h1>
-    </main>
-  );
+  return <main>homepage</main>;
 }
