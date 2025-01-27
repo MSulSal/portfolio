7bfaf378899b71488026a1c101ea5ae4c07360b9
MSulSal <129910316+MSulSal@users.noreply.github.com>
2025-01-26 22:29:33 -0600

    add resume page

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -4,10 +4,10 @@ import { BsArrowDownRight } from "react-icons/bs";
 import Link from "next/link";
 import { motion } from "framer-motion";
 
-const services = [
+const projects = [
   {
     num: "01",
-    title: "Web Devlopment",
+    title: "Web Development",
     description: "Lorem ipsum dolor sit amet consectetur, adipisicing elit.",
     href: "",
   },
@@ -37,7 +37,7 @@ const services = [
   },
 ];
 
-const Services = () => {
+const Projects = () => {
   return (
     <section className="min-h-[80vh] flex flex-col justify-center py-12 xl:py-0">
       <div className="container mx-auto">
@@ -49,7 +49,7 @@ const Services = () => {
           }}
           className="grid grid-cols-1 md:grid-cols-2 gap-[60px]"
         >
-          {services.map((service, index) => {
+          {projects.map((projectType, index) => {
             return (
               <div
                 key={index}
@@ -57,15 +57,15 @@ const Services = () => {
               >
                 {/* top */}
                 <div className="w-full flex justify-between items-center">
-                  <div className="text-5xl font-extrabold">{service.num}</div>
-                  <Link href={service.href}>
-                    <BsArrowDownRight />
+                  <div className="text-5xl font-extrabold text-outline text-transparent group-hover:text-outline-hover transition-all duration-500">{projectType.num}</div>
+                  <Link href={projectType.href} className="w-[70px] h-[70px] rounded-full bg-white group-hover:bg-accent transition-all duration-500 flex justify-center items-center hover:-rotate-45">
+                    <BsArrowDownRight className="text-primary text-3xl" />
                   </Link>
                 </div>
                 {/* title */}
-                <h2>{service.title}</h2>
+                <h2 className="text-[42px] font-bold leading-none text-white group-hover:text-accent transition-all duration-500">{projectType.title}</h2>
                 {/* description */}
-                <p>{service.description}</p>
+                <p className="text-white/60">{projectType.description}</p>
                 {/* border */}
                 <div className="border-b border-white/20 w-full"></div>
               </div>
@@ -77,4 +77,4 @@ const Services = () => {
   );
 };
 
-export default Services;
+export default Projects;
