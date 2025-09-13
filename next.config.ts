1e9558bb77a3dcd448e54f18e5ca03a25289fd9e
MSulSal <129910316+MSulSal@users.noreply.github.com>
2025-09-13 07:45:33 -0500

    new portfolio

--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  output: "export",
-  distDir: "dist",
-  trailingSlash: true,
-  images: {
-    unoptimized: true, // Disable image optimization for static export
-  },
+  /* config options here */
 };
 
 export default nextConfig;
